48d039da9b1cbf6ce92dbbbeeeb325ade6ea61fb
Chad-Vine-Doll <164040662@qq.com>
2024-10-01 05:55:15 -0500

    Add add force

---
+++ Runtime/Nodes/Calls/AddForce.cs
@@ -0,0 +1,25 @@
+﻿using Amlos.AI.Variables;
+using UnityEngine;
+
+namespace Amlos.AI.Nodes
+{
+    public class AddForce : Call
+    {
+        public VariableField<Vector2> force;
+
+        Rigidbody2D rb;
+
+        public override State Execute()
+        {
+            if (!rb) return State.Failed;
+
+            rb.AddForce(force);
+            return State.Success;
+        }
+
+        public override void Initialize()
+        {
+            rb = gameObject.GetComponent<Rigidbody2D>();
+        }
+    }
+}
