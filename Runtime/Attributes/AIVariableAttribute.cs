576fc7a7cd017cda0262d1edd2b4c70f91d4973d
Chad-Vine-Doll <164040662@qq.com>
2025-08-06 20:51:29 -0400

    AI var progress

---
+++ Runtime/Attributes/AIVariableAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Minerva.Module;
+using System;
+
+namespace Amlos.AI
+{
+    [System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+    public sealed class AIVariableAttribute : Attribute
+    {
+        public string name;
+        public UUID? uuid;
+
+        public AIVariableAttribute()
+        {
+        }
+
+        public AIVariableAttribute(string name = null, UUID? uuid = null)
+        {
+            this.name = name;
+            this.uuid = uuid;
+        }
+    }
+}
