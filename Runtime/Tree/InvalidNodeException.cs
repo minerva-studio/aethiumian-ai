7174b9fc90c61ef548cfb75a4cee6135add7dbd7
Henrymei2 <130747819+Henrymei2@users.noreply.github.com>
2025-12-09 22:03:19 -0600

    Update InvalidNodeException.cs

--- Runtime/Tree/InvalidNodeException.cs
+++ Runtime/Tree/InvalidNodeException.cs
@@ -1,38 +1,77 @@
 ﻿using System;
 using System.Runtime.Serialization;
+using Amlos.AI.Nodes;
 
 namespace Amlos.AI
 {
     [Serializable]
     public class InvalidNodeException : Exception
     {
+        public Type NodeType { get; }
+
         public InvalidNodeException() { }
-        public InvalidNodeException(string message) : base(message) { }
-        public InvalidNodeException(string message, Exception inner) : base(message, inner) { }
-        protected InvalidNodeException(
-          SerializationInfo info,
-          StreamingContext context) : base(info, context) { }
 
-
-
-        public static InvalidNodeException VariableIsRequired(string varName)
+        public InvalidNodeException(string message, Type nodeType = null)
+            : base(AppendNodeName(message, nodeType))
         {
-            return new InvalidNodeException($"Variable \"{varName}\" is required");
+            NodeType = nodeType;
         }
 
-        public static InvalidNodeException InvalidValue(string varName, object value)
+        public InvalidNodeException(string message, Exception inner, Type nodeType = null)
+            : base(AppendNodeName(message, nodeType), inner)
         {
-            return new InvalidNodeException($"Variable \"{varName}\" has invalid value: {value}");
+            NodeType = nodeType;
         }
 
-        public static InvalidNodeException InvalidValue(VariableType type, object value)
+        protected InvalidNodeException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
+
+        private static string AppendNodeName(string msg, Type nodeType)
         {
-            return new InvalidNodeException($"Variable Type \"{type}\" has invalid value: {value}");
+            if (nodeType == null) return msg;
+            return $"[{nodeType.Name}] {msg}";
         }
 
-        public static InvalidNodeException ReferenceIsRequired(string varName)
+        public static InvalidNodeException VariableIsRequired(string varName, TreeNode node)
         {
-            return new InvalidNodeException($"Children \"{varName}\" is required");
+            return new InvalidNodeException(
+                $"Variable \"{varName}\" is required",
+                node.GetType()
+            );
+        }
+
+        // VariableBase throws InvalidOperationException instead
+        // public static InvalidNodeException InvalidValue(string varName, object value, TreeNode node)
+        // {
+        //     return new InvalidNodeException(
+        //         $"Variable \"{varName}\" has invalid value: {value}",
+        //         node.GetType()
+        //     );
+        // }
+
+        // public static InvalidNodeException InvalidValue(VariableType type, object value, TreeNode node)
+        // {
+        //     return new InvalidNodeException(
+        //         $"Variable Type \"{type}\" has invalid value: {value}",
+        //         node.GetType()
+        //     );
+        // }
+
+        public static InvalidNodeException InvalidValue(string message, TreeNode node)
+        {
+            return new InvalidNodeException(
+                message,
+                node.GetType()
+            );
+        }
+
+        public static InvalidNodeException ReferenceIsRequired(string varName, TreeNode node)
+        {
+            return new InvalidNodeException(
+                $"Children \"{varName}\" is required",
+                node.GetType()
+            );
         }
     }
+
 }
